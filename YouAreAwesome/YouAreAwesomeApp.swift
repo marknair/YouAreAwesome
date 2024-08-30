62b29580a0abb1c6cfbbbdb86e2e2f5d7bc19e4b
Mark Nair <47827647+marknair@users.noreply.github.com>
2024-08-29 20:06:27 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Mark Nair on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
